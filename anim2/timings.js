8b4806e4d62cb3aeabba48836556b8af0aaf0642
Svante Henriksson <svante.henriksson@gmail.com>
2026-04-26 22:33:20 +0300

    update

--- anim2/timings.js
+++ anim2/timings.js
@@ -1,28 +1,15 @@
 export const timings = [
-        "left": 0.00, 1.36
-        "quiet": 1.36, 1.86
-        "right": 1.86, 2.76
-        "quiet": 2.76, 3.44
-        "left": 3.44, 5.04
-        "quiet": 5.04, 5.98
-        "right": 5.98, 7.26
-        "quiet": 7.26, 7.80
-        "left": 7.80, 8.96
-        "quiet": 8.96, 9.76
-        "right": 9.76, 10.22
-    ];
+  { state: "left", start: 0.0, end: 1.36 },
+  { state: "quiet", start: 1.36, end: 1.86 },
+  { state: "right", start: 1.86, end: 2.76 },
+  { state: "quiet", start: 2.76, end: 3.44 },
+  { state: "left", start: 3.44, end: 5.04 },
+  { state: "quiet", start: 5.04, end: 5.98 },
+  { state: "right", start: 5.98, end: 7.26 },
+  { state: "quiet", start: 7.26, end: 7.8 },
+  { state: "left", start: 7.8, end: 8.96 },
+  { state: "quiet", start: 8.96, end: 9.76 },
+  { state: "right", start: 9.76, end: 10.22 },
+];
 
-
-
- //Hyvää: 0.00s – 0.48s
- //huomenta.: 0.48s – 1.36s
- //Huomenta.: 1.86s – 2.76s
- //Mikä: 3.44s – 3.64s
- //on: 3.64s – 3.78s
- //päivän: 3.78s – 4.08s
- //ykkösuutinen?: 4.08s – 5.04s
- //Kylmä: 5.98s – 6.48s
- //sää.: 6.48s – 7.26s
- //Kuulostaa: 7.80s – 8.24s
- //vakavalta.: 8.24s – 8.96s
- //Kyllä.: 9.76s – 10.22s
+export default timings;
