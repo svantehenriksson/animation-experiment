5360ad4c44172759084491d12f6f4c9b89c550ea
Svante Henriksson <svante.henriksson@gmail.com>
2026-04-26 23:01:52 +0300

    update

---
+++ anim2/subtitles_raw.js
@@ -0,0 +1,15 @@
+export const subtitlesRaw = [
+  { fi: "Hyvaa huomenta", en: "Good morning", start: 0.0, end: 1.36 },
+  { fi: "Huomenta", en: "Morning", start: 1.86, end: 2.76 },
+  {
+    fi: "Mika on paivan ykkosuutinen?",
+    en: "What is the day's number one news?",
+    start: 3.44,
+    end: 5.04,
+  },
+  { fi: "Kylma saa", en: "Cold weather", start: 5.98, end: 7.26 },
+  { fi: "Kuulostaa vakavalta", en: "Sounds serious", start: 7.8, end: 8.96 },
+  { fi: "Kyllä", en: "Yes", start: 9.76, end: 10.22 },
+];
+
+export default subtitlesRaw;
